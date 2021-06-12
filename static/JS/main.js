dc9beb6a090f45ded6721e8ba6a199214536ffcc
Moringa School <kelvin.bett@student.moringaschool.com>
2021-06-12 16:58:19 +0300

    this will add the variables declaration

---
+++ static/JS/main.js
@@ -0,0 +1,4 @@
+var CC, YY, MM, DD, d, dayValue;
+var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "kofi", "kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
