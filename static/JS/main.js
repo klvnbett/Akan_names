a9469025d3efde727576b3fd7e505bbaeb24647c
Moringa School <kelvin.bett@student.moringaschool.com>
2021-06-14 17:23:45 +0300

    commiting formula for gender

--- static/JS/main.js
+++ static/JS/main.js
@@ -36,9 +36,9 @@ function calculateDayValue() {
     YY = parseInt(year.substring(2, 4));
     MM = parseInt(document.getElementById("month").value);
     DD = parseInt(document.getElementById("date").value);
-    d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * MM + 1) / 10)) + DD) % 7;
-console.log(d);
-return (Math.floor(d));
+    d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * MM + 1) / 10) + DD) % 7;
+    console.log(d);
+    return (Math.floor(d));
 }
 
 function getGender() {
@@ -97,6 +97,13 @@ function getGender() {
             if (dayValue == -0) {
                 alert("Your AKAN Name is " + femaleNames[6] + "." + "since you were born on " + weekDays[6]);
             }
-    }
 
+            break
+        default:
+    }
+}
+
+function findName() {
+    dayValue.calculateDayValue();
+    getGender();
 }
